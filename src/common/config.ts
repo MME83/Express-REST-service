f80cde75aa88f4e8c5afc6dc43068ca27d7d3473
Mykola Moroz <moroz.mykola@gmail.com>
2021-06-26 14:57:41 +0300

    feat: add typeORM config

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 import dotenv from 'dotenv';
 import path from 'path';
 
@@ -5,11 +6,18 @@ dotenv.config({
   path: path.join(__dirname, '../../.env')
 });
 
+export default {
+  PORT: process.env['PORT'],
+  NODE_ENV: process.env['NODE_ENV'],
+  JWT_SECRET_KEY: process.env['JWT_SECRET_KEY'],
+  AUTH_MODE: process.env['AUTH_MODE'] === 'true',
+}
+
+/*
 export const { PORT } = process.env;
 export const { NODE_ENV } = process.env;
-export const { MONGO_CONNECTION_STRING } = process.env;
+// export const { MONGO_CONNECTION_STRING } = process.env;
 export const { JWT_SECRET_KEY } = process.env;
-
 const { AUTH_MODE: authMode } = process.env;
-
 export const AUTH_MODE = authMode === 'true';
+*/
---
+++ src/common/ormconfig.ts
@@ -0,0 +1,19 @@
+/* eslint-disable dot-notation */
+import { ConnectionOptions } from 'typeorm';
+
+export const options: ConnectionOptions = {
+    type: 'postgres',
+    host: process.env['DB_HOST'],
+    port: Number(process.env['POSTGRES_PORT']),
+    username: process.env['POSTGRES_USER'],
+    password: process.env['POSTGRES_PASSWORD'],
+    database: process.env['POSTGRES_DB'],
+    entities: ['../../src/entities/**/*.ts'],
+    logging: Boolean(process.env['DB_LOGGING']),
+    synchronize: Boolean(process.env['DB_SYNC']),
+    migrations: ['../../src/migrations/*.ts'],
+    cli: {
+        entitiesDir: '../../src/entities',
+        migrationsDir: '../../src/migrations',
+    },
+};
