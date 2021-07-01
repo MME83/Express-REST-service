7d9e35332623fe82f914d7cc86bcb22abd9d256c
Mykola Moroz <moroz.mykola@gmail.com>
2021-07-01 14:10:23 +0300

    fix: fix connect and start method on server with new dbconnect option

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
-import { /* Connection, */ createConnection } from 'typeorm';
+// import { createConnection } from 'typeorm';
 import config from './common/config';
-import configTypeorm from './common/ormconfig';
+// import configTypeorm from './common/ormconfig';
 import app from './app';
 import { logger } from './logger/logger';
+import { dbCreateConnection } from './common/dbCreateCOnnection';
 
-console.log(configTypeorm);
+/*
 createConnection(configTypeorm)
-  .then(async (connection) => {
+  .then(async connection => {
     if (connection.isConnected) {
       process.stdout.write('DB connection has succeeded\n');
       logger.info({ message: 'DB connection has succeeded\n' });
@@ -20,9 +21,22 @@ createConnection(configTypeorm)
       connection.connect();
     }
   })
-  .catch((error) => {
+  .catch((error: Error) => {
+    console.log(`Fail connect to DB: `, error.message);
     logger.error({
       message: `Caught exception ${error} Exception from server.ts\n`,
     });
     logger.exitOnError = true;
 });
+*/
+
+const start = async () => {
+  await dbCreateConnection();
+
+  app.listen(config.PORT, () => {
+    process.stdout.write(`App is running on http://localhost:${config.PORT}\n`);
+    logger.info(`App is running on http://localhost:${config.PORT}\n`);
+  });
+};
+
+start().catch(console.error);
