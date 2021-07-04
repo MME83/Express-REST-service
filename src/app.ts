a2e95af8be4cb873d3a5c231164bd1d315b5678b
Mykola Moroz <moroz.mykola@gmail.com>
2021-07-04 21:20:06 +0300

    feat: add login router to app

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
 
+import loginRouter from './resources/login/login.router';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
@@ -26,6 +27,7 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use('/login', loginRouter);
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards/:boardId/tasks', taskRouter);
